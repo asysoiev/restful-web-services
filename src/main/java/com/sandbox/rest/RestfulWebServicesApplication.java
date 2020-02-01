f1d5d9c94f68f4369f35e7aafe333fa1db9943d6
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2020-02-01 09:01:56 +0200

    Trimmed base package

--- src/main/java/com/sandbox/rest/RestfulWebServicesApplication.java
+++ src/main/java/com/sandbox/rest/RestfulWebServicesApplication.java
@@ -1,4 +1,4 @@
-package com.sandbox.rest.webservices;
+package com.sandbox.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
