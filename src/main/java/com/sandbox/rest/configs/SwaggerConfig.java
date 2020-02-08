e6cc4419d92422d4c8bbea5c6553e44f672e9ae6
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2020-02-08 15:32:57 +0200

    SM: enhanced swagger documentation

--- src/main/java/com/sandbox/rest/configs/SwaggerConfig.java
+++ src/main/java/com/sandbox/rest/configs/SwaggerConfig.java
@@ -4,9 +4,20 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
 /**
  * @author Andrii Sysoiev
@@ -16,10 +27,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAutoConfiguration(exclude = {HypermediaAutoConfiguration.class})
 public class SwaggerConfig {
 
+    public static final Contact CONTACT = new Contact("Andrii Sysoiev", "", "andrii.sysoiev@gmail.com");
+    private static final ApiInfo API_INFO = new ApiInfo("Sandbox web service", "RESTfull web service samples", "1.0", "urn:tos",
+            CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
+    private static final Set<String> PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList(APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE));
+
     @Bean
     public Docket api() {
-        Docket docket = new Docket(DocumentationType.SWAGGER_2);
-        return docket;
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(API_INFO)
+                .consumes(PRODUCES_AND_CONSUMES)
+                .produces(PRODUCES_AND_CONSUMES)
+                ;
     }
 
 }
--- src/main/java/com/sandbox/rest/social/models/User.java
+++ src/main/java/com/sandbox/rest/social/models/User.java
@@ -1,5 +1,8 @@
 package com.sandbox.rest.social.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -8,12 +11,15 @@ import java.util.StringJoiner;
 /**
  * @author Andrii Sysoiev
  */
+@ApiModel(description = "Sample model for experiments")
 public class User {
 
     private Integer id;
-    @Size(min = 2, message = "Name should have at least 2 characters")
+    @Size(min = 2, message = "Name must have at least 2 characters")
+    @ApiModelProperty(value = "Name must have at least 2 characters")
     private String name;
     @Past
+    @ApiModelProperty(value = "Must be in the past")
     private LocalDateTime birthDate;
 
     public Integer getId() {
