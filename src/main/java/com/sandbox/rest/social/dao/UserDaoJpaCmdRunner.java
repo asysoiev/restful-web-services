fcfec61ecfdae5a943a0055d19cca2e04ee0a602
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2020-02-14 19:32:29 +0200

    SM: added UserDaoJpaCmdRunner

---
+++ src/main/java/com/sandbox/rest/social/dao/UserDaoJpaCmdRunner.java
@@ -0,0 +1,35 @@
+package com.sandbox.rest.social.dao;
+
+import com.sandbox.rest.social.models.User;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author Andrii Sysoiev
+ */
+@Profile("jpa")
+@Component
+public class UserDaoJpaCmdRunner implements CommandLineRunner {
+
+    private static final Logger logger = getLogger(UserDaoJpaCmdRunner.class);
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User adam = userDao.createUser(new User("Adam", LocalDateTime.now().minusYears(10)));
+        logger.debug("Created new user: {}", adam);
+        User eve = userDao.createUser(new User("Eve", LocalDateTime.now().minusYears(30)));
+        logger.debug("Created new user: {}", eve);
+        User jack = userDao.createUser(new User("Jack", LocalDateTime.now().minusYears(20)));
+        logger.debug("Created new user: {}", jack);
+    }
+}
